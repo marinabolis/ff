2738e107e2ac9a46835d99c859e69c26be850c91
marinabolis <MarinaBahgatBolis25@outlook.com>
2023-03-10 21:55:42 +0200

    test

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+username: string="mohamed"
+userAge:number =32
+images: string="assets/images/20200226_094020.jpg"
+imgWidth:number = 500
+
+}
+
+
+
